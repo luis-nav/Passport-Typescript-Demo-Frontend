5e3a866fa2d11d36143141c7b1f3f4d4d77638fd
unknown <luiscatodd@gmail.com>
2022-02-01 15:32:07 -0600

    Created pages and basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import AdminPage from "./pages/AdminPage";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path='' element={<Homepage />} />
+                <Route path='/admin' element={<AdminPage />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/profile' element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    )
+};
 
 export default App;
